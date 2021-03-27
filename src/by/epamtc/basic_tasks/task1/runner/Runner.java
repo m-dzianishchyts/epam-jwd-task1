cc020384380ab576cfb4ac430bac8280ab283896
maxiemar <denishicmihail@gmail.com>
2021-03-27 20:00:33 +0300

    Make it in english

--- src/by/epamtc/basic_tasks/task1/runner/Runner.java
+++ src/by/epamtc/basic_tasks/task1/runner/Runner.java
@@ -6,10 +6,10 @@ import by.epamtc.basic_tasks.task1.unit.LastDigitLogics;
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.print("Введите целое число: ");
+        System.out.print("Enter integer number: ");
         int number = DataScanner.enterIntegerFromConsole();
 
         int lastDigitOfSquare = LastDigitLogics.calculateLastDigitOfSquareOf(number);
-        System.out.printf("Последняя цифра квадрата числа %d: %d\n", number, lastDigitOfSquare);
+        System.out.printf("Last digit of square of %d: %d\n", number, lastDigitOfSquare);
     }
 }
--- src/by/epamtc/basic_tasks/task2/runner/Runner.java
+++ src/by/epamtc/basic_tasks/task2/runner/Runner.java
@@ -1,20 +1,21 @@
 package by.epamtc.basic_tasks.task2.runner;
 
-import by.epamtc.basic_tasks.task2.scanner.DateScanner;
+import by.epamtc.basic_tasks.scanner.DataScanner;
+import by.epamtc.basic_tasks.task2.scanner.MonthScanner;
 import by.epamtc.basic_tasks.task2.unit.DateLogics;
 import by.epamtc.basic_tasks.task2.unit.Month;
 
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.print("Введите год: ");
-        int year = DateScanner.enterYearFromConsole();
-        System.out.print("Введите номер месяца: ");
-        Month month = DateScanner.enterMonthFromConsole();
+        System.out.print("Enter year: ");
+        int year = DataScanner.enterPositiveIntegerFromConsole();
+        System.out.print("Enter month number: ");
+        Month month = MonthScanner.enterMonthFromConsole();
 
         int daysInMonthAmount = DateLogics.retrieveDaysAmountInMonth(year, month);
 
-        // Смещение в номере месяца из-за нумерации с нуля.
-        System.out.printf("Количество дней в %d месяце %d года: %d\n", month.ordinal() + 1, year, daysInMonthAmount);
+        // Offset in month number because of elements numbering from 0.
+        System.out.printf("Amount of days in the %d month of %d: %d\n", month.ordinal() + 1, year, daysInMonthAmount);
     }
 }
