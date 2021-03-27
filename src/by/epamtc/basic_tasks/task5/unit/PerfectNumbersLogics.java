ce3f4aa6e8384599776f081c354c66e1c50a845e
maxiemar <denishicmihail@gmail.com>
2021-03-27 23:29:56 +0300

    Fix perfect number logics.

--- src/by/epamtc/basic_tasks/task5/unit/PerfectNumbersLogics.java
+++ src/by/epamtc/basic_tasks/task5/unit/PerfectNumbersLogics.java
@@ -20,31 +20,30 @@ public class PerfectNumbersLogics {
      * Uses absolute value of the number.
      */
     public static long calculateDivisorsSum(long number) {
+
+        // Work with absolute value.
+        long numberAbs = Math.abs(number);
+
         long divisorsSum;
-        if (number == 0) {
+        if (numberAbs == 0) {
 
             // 0 has an infinite number of divisors.
             divisorsSum = Long.MAX_VALUE;
-        } else if (number == 1) {
+        } else if (numberAbs == 1) {
 
             // The only divisor of 1 is 1, but it does not fit.
             divisorsSum = 0;
         } else {
 
-            // Work with absolute value.
-            if (number < 0) {
-                number = -number;
-            }
-
             // Each number is divisible by 1, so it can start with 1.
             // It will detect divisors faster by adding the divisor itself to the sum
             // and 'number / divisor' (which is also divisor).
             divisorsSum = 1;
-            for (long i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0) {
+            for (long i = 2; i <= Math.sqrt(numberAbs); i++) {
+                if (numberAbs % i == 0) {
                     divisorsSum += i;
-                    if (i * i != number) {
-                        divisorsSum += number / i;
+                    if (i * i != numberAbs) {
+                        divisorsSum += numberAbs / i;
                     }
                 }
             }
