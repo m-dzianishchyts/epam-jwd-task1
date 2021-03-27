7d6774e45cf7b696213bd0910248ff930d6a8747
maxiemar <denishicmihail@gmail.com>
2021-03-28 02:38:47 +0300

    Fix function

--- src/by/epamtc/basic_tasks/task8/unit/SomeFunction.java
+++ src/by/epamtc/basic_tasks/task8/unit/SomeFunction.java
@@ -2,12 +2,28 @@ package by.epamtc.basic_tasks.task8.unit;
 
 public class SomeFunction {
 
-    private static final Function functionOptionA = x -> -x * x + 3 * x + 9;
-    private static final Function functionOptionB = x -> {
-        if (x == Math.pow(6, 1.0 / 3)) {
-            throw new NoFunctionValueException("Division by zero.");
+    private static final Function functionOptionA = x -> {
+        double value;
+        if (Double.isInfinite(x) || Double.isNaN(x)) {
+            value = -x;
+        } else {
+            value = -x * x + 3 * x + 9;
         }
-        return 1 / (x * x * x - 6);
+        return value;
+    };
+
+    private static final Function functionOptionB = x -> {
+        double value;
+        if (Double.isInfinite(x)) {
+            value = 0;
+        } else if (Double.isNaN(x)) {
+            value = Double.NaN;
+        } else if (x == Math.pow(6, 1.0 / 3)) {
+            throw new NoFunctionValueException("Division by zero.");
+        } else {
+            value = 1 / (x * x * x - 6);
+        }
+        return value;
     };
 
     public static double valueAt(double x) throws NoFunctionValueException {
