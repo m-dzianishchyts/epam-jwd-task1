7e16ef5b196c1d1866f8fd637c97e875007f5689
maxiemar <denishicmihail@gmail.com>
2021-04-04 19:51:28 +0300

    Add isEven() reason

--- src/by/epamtc/basic_tasks/task4/unit/EvenNumbersLogics.java
+++ src/by/epamtc/basic_tasks/task4/unit/EvenNumbersLogics.java
@@ -2,6 +2,10 @@ package by.epamtc.basic_tasks.task4.unit;
 
 public class EvenNumbersLogics {
 
+    // This method seems acceptable to me,
+    // because the client of the code will not
+    // have to keep in mind the definition of what an 'even number' is.
+    // This applies to any definition, even one as simple as an 'even number'.
     public static boolean isEven(int number) {
         boolean isEven = number % 2 == 0;
         return isEven;
