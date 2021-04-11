3aa0070d3b2eeee63f48348d7f9918b4ab3c003a
maxiemar <denishicmihail@gmail.com>
2021-04-11 15:38:08 +0300

    Extract radius check

--- src/by/epamtc/basic_tasks/task9/unit/CircleLogics.java
+++ src/by/epamtc/basic_tasks/task9/unit/CircleLogics.java
@@ -3,18 +3,20 @@ package by.epamtc.basic_tasks.task9.unit;
 public class CircleLogics {
 
     public static double calculatePerimeterByRadius(double radius) throws InvalidCirclePropertyException {
-        if (radius < 0) {
-            throw new InvalidCirclePropertyException("Circle radius cannot be negative.");
-        }
+        checkRadius(radius);
         double perimeter = 2 * Math.PI * radius;
         return perimeter;
     }
 
     public static double calculateAreaByRadius(double radius) throws InvalidCirclePropertyException {
+        checkRadius(radius);
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+
+    private static void checkRadius(double radius) throws InvalidCirclePropertyException {
         if (radius < 0) {
             throw new InvalidCirclePropertyException("Circle radius cannot be negative.");
         }
-        double area = Math.PI * radius * radius;
-        return area;
     }
 }
