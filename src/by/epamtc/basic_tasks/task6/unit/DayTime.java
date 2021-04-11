b2289fb1d3131aa6ecaeab90cf38397f1837b997
maxiemar <denishicmihail@gmail.com>
2021-04-11 15:18:37 +0300

    Use time constants

--- src/by/epamtc/basic_tasks/task6/unit/DayTime.java
+++ src/by/epamtc/basic_tasks/task6/unit/DayTime.java
@@ -5,20 +5,24 @@ package by.epamtc.basic_tasks.task6.unit;
  */
 public class DayTime {
 
+    public static final int HOURS_PER_DAY = 24;
+    public static final int MINUTES_PER_HOUR = 60;
+    public static final int SECONDS_PER_MINUTE = 60;
+
     private int hours;
     private int minutes;
     private int seconds;
 
     public DayTime(int hours, int minutes, int seconds) throws InvalidTimeException {
-        if (hours < 0 || hours > 23) {
-            throw new InvalidTimeException("Hour must be between 0 and 23");
+        if (hours < 0 || hours >= HOURS_PER_DAY) {
+            throw new InvalidTimeException("Hour must be between 0 and " + (HOURS_PER_DAY - 1) + ".");
         }
-        if (minutes < 0 || minutes > 59) {
-            throw new InvalidTimeException("Minute must be between 0 and 59");
+        if (minutes < 0 || minutes >= MINUTES_PER_HOUR) {
+            throw new InvalidTimeException("Minute must be between 0 and " + (MINUTES_PER_HOUR - 1) + ".");
         }
         // A leap second is allowed.
-        if (seconds < 0 || seconds > 60) {
-            throw new InvalidTimeException("Second must be between 0 and 60");
+        if (seconds < 0 || seconds > SECONDS_PER_MINUTE) {
+            throw new InvalidTimeException("Second must be between 0 and " + SECONDS_PER_MINUTE + ".");
         }
         this.hours = hours;
         this.minutes = minutes;
@@ -64,23 +68,23 @@ public class DayTime {
     }
 
     public void setHours(int hours) throws InvalidTimeException {
-        if (hours < 0 || hours > 23) {
-            throw new InvalidTimeException("Hour must be between 0 and 23");
+        if (hours < 0 || hours >= HOURS_PER_DAY) {
+            throw new InvalidTimeException("Hour must be between 0 and " + (HOURS_PER_DAY - 1) + ".");
         }
         this.hours = hours;
     }
 
     public void setMinutes(int minutes) throws InvalidTimeException {
-        if (minutes < 0 || minutes > 59) {
-            throw new InvalidTimeException("Minute must be between 0 and 59");
+        if (minutes < 0 || minutes >= MINUTES_PER_HOUR) {
+            throw new InvalidTimeException("Minute must be between 0 and " + (MINUTES_PER_HOUR - 1) + ".");
         }
         this.minutes = minutes;
     }
 
     public void setSeconds(int seconds) throws InvalidTimeException {
         // A leap second is allowed.
-        if (seconds < 0 || seconds > 60) {
-            throw new InvalidTimeException("Second must be between 0 and 60");
+        if (seconds < 0 || seconds > SECONDS_PER_MINUTE) {
+            throw new InvalidTimeException("Second must be between 0 and " + SECONDS_PER_MINUTE + ".");
         }
         this.seconds = seconds;
     }
