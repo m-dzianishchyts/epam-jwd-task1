ab2864d0a997275c02b39c9dc90c95153dc107f8
maxiemar <denishicmihail@gmail.com>
2021-04-11 15:18:59 +0300

    Consider leap second

--- src/by/epamtc/basic_tasks/task6/unit/TimeLogics.java
+++ src/by/epamtc/basic_tasks/task6/unit/TimeLogics.java
@@ -2,23 +2,28 @@ package by.epamtc.basic_tasks.task6.unit;
 
 public class TimeLogics {
 
-    private static final int SECONDS_PER_MINUTE = 60;
-    private static final int MINUTES_PER_HOUR = 60;
-    private static final int HOURS_PER_DAY = 24;
-
     public static DayTime calculateDayTimeByCurrentSecond(int secondNumber) throws InvalidTimeException {
         if (secondNumber <= 0) {
             throw new InvalidTimeException("Current second number must be positive.");
         }
-        int secondsPerDay = SECONDS_PER_MINUTE * MINUTES_PER_HOUR * HOURS_PER_DAY;
-        if (secondNumber > secondsPerDay) {
-            throw new InvalidTimeException("Current second number cannot be greater then " + secondsPerDay + ".");
+        int secondsPerDay = DayTime.SECONDS_PER_MINUTE * DayTime.MINUTES_PER_HOUR * DayTime.HOURS_PER_DAY;
+
+        // Remember one possible leap second.
+        if (secondNumber > secondsPerDay + 1) {
+            throw new InvalidTimeException("Current second number cannot be greater then " + (secondsPerDay + 1) + ".");
         }
         int passedSecondsAmount = secondNumber - 1;
-        int secondsPerHour = SECONDS_PER_MINUTE * MINUTES_PER_HOUR;
+
+        // Remove leap second, if any, to calculate day time correctly.
+        int leapSecond = passedSecondsAmount / secondsPerDay;
+        passedSecondsAmount -= leapSecond;
+
+        int secondsPerHour = DayTime.SECONDS_PER_MINUTE * DayTime.MINUTES_PER_HOUR;
         int passedTimeHours = passedSecondsAmount / secondsPerHour;
-        int passedTimeMinutes = (passedSecondsAmount % secondsPerHour) / SECONDS_PER_MINUTE;
-        int passedTimeSeconds = passedSecondsAmount % SECONDS_PER_MINUTE;
+        int passedTimeMinutes = (passedSecondsAmount % secondsPerHour) / DayTime.SECONDS_PER_MINUTE;
+
+        // Consider leap second now.
+        int passedTimeSeconds = passedSecondsAmount % DayTime.SECONDS_PER_MINUTE + leapSecond;
         DayTime dayTime = new DayTime(passedTimeHours, passedTimeMinutes, passedTimeSeconds);
         return dayTime;
     }
