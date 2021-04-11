7ef21e0e4914030ac9b818441e0fefe0c26268c2
maxiemar <denishicmihail@gmail.com>
2021-04-11 15:49:07 +0300

    Fix NaN and infinite interval bounds

--- src/by/epamtc/basic_tasks/task10/unit/TangentValuesLogics.java
+++ src/by/epamtc/basic_tasks/task10/unit/TangentValuesLogics.java
@@ -3,21 +3,24 @@ package by.epamtc.basic_tasks.task10.unit;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class TangentValuesLogics {
+public final class TangentValuesLogics {
+
+    private TangentValuesLogics() {
+    }
 
     public static Map<Double, Double> computeTangentValues(double fromValue, double toValue, double step)
             throws InvalidIntervalException {
-        if (toValue < fromValue) {
+        if (!Double.isFinite(fromValue) || !Double.isFinite(toValue) || toValue < fromValue) {
             throw new InvalidIntervalException("Invalid interval.");
         }
         if (step <= 0) {
             throw new InvalidIntervalException("Step must be positive.");
         }
-        Map<Double, Double> functionValues = new LinkedHashMap<>();
-        for (double x = fromValue; x < toValue; x += step) {
-            double functionValue = Tangent.valueAt(x);
-            functionValues.put(x, functionValue);
+        Map<Double, Double> tangentValues = new LinkedHashMap<>();
+        for (double x = fromValue; x <= toValue; x += step) {
+            double tangentValue = Tangent.valueAt(x);
+            tangentValues.put(x, tangentValue);
         }
-        return functionValues;
+        return tangentValues;
     }
 }
